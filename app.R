38b2a1778b8ff69503859b32b1afdbccf55d3fce
Blackhawkjag <92227892+Blackhawkjag@users.noreply.github.com>
2021-10-11 20:57:44 -0500

    Update app.R
    Revision 1.1

--- app.R
+++ app.R
@@ -1,62 +1,65 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-1
-library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+library(shiny)
+library(shinythemes)
+
+# Reserve Ui for application
+ui <- fluidPage(theme = shinytheme("superhero"),
 
     # Application title
     titlePanel("EasyCharts!
                "),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            tags$h3("BoxPlots!"),
-            textInput("Data1","Data Point 1", ""),
-            textInput("Data2","Data Point 2", ""),
-            textInput("BoxplotTitle", "Boxplot Title", ""),
-            textInput("Boxplotxlab", "X-Label", ""),
-            textInput("Boxplotylab", "Y-Label", ""),
-            
-        ),
+    # First Window for Box Plots
+    navbarPage("Plots, Graphs, and Charts:",
+               tabPanel("BoxPlot",
+                        sidebarLayout(
+                          sidebarPanel(
+                            tags$h3("BoxPlots!"),
+                            textInput("Data1","Data Point 1", ""),
+                            textInput("Data2","Data Point 2", ""),
+                            textInput("BoxplotTitle", "Boxplot Title", ""),
+                            textInput("Boxplotxlab", "X-Label", ""),
+                            textInput("Boxplotylab", "Y-Label", ""),
+                            selectInput("ColorChoice", "Choose Color1", c("Red", "Green","Blue")),
+                            selectInput("ColorChoice2","Pick Second Color", c("Red", "Green","Blue")),
+                            
+                          ),
+                      
+                        
 
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot")
-        )
-    )
-)
+        )))))
+        
+        
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x <- as.integer(strsplit(input$Data1, ",")[[1]])
+        # Set variables for dual input box plot
+        boxplotv1 <- as.integer(strsplit(input$Data1, ",")[[1]])
         
-        y <- as.integer(strsplit(input$Data2, ",")[[1]])
+        boxplotv2 <- as.integer(strsplit(input$Data2, ",")[[1]])
         
+        boxcolor1 <- input$ColorChoice
+        boxcolor2 <- input$ColorChoice2
         
       
-                                 
 
-        # draw the histogram with the specified number of bins
-        boxplot(x,y,
+        # draw boxplot
+        boxplot(boxplotv1,boxplotv2,
                 ylim=c(0,50),
                 main=input$BoxplotTitle,
                 xlab=input$Boxplotxlab,
                 ylab=input$Boxplotylab,
-                col=c("Purple","Red"))
+                col=c(boxcolor1,boxcolor2))
     })
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+
+
+shinyApp(ui = ui,  server = server)
+
