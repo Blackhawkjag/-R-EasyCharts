6c6a8cccd2f04ac71c9b9dcf88132ca5ec494d22
Blackhawkjag <92227892+Blackhawkjag@users.noreply.github.com>
2021-10-11 22:33:13 -0500

    Update app.R
    Revision 1.2

--- app.R
+++ app.R
@@ -28,11 +28,32 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                         
 
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )))))
+        mainPanel(plotOutput("distPlot")))),
+        tabPanel("LinePlot",
+                   sidebarPanel(
+                     tags$h3("LinePlots!"),
+                     textInput("LineData1","Data Point 1", ""),
+                     textInput("LineData2","Data Point 2", ""),
+                     textInput("linexlab", "X-Lab", ""),
+                     textInput("lineylab", "Y-Lab", ""),
+                   ),
+                 mainPanel(plotOutput("lineplot"))),
+        
+        tabPanel("BarPlot",
+                 sidebarPanel(
+                   tags$h3("BarPlots!"),
+                   textInput("BoxData1","Data Point 1", ""),
+                   textInput("BoxData2","Title", ""),
+                   textInput("BoxData3","Xlab", ""),
+                   textInput("BoxData4","ylab", ""),
+                 ),
+                 mainPanel(plotOutput("boxplot")))))
+                   
+                 
         
         
+  
+
 
 # Define server logic 
 server <- function(input, output) {
@@ -56,10 +77,50 @@ server <- function(input, output) {
                 ylab=input$Boxplotylab,
                 col=c(boxcolor1,boxcolor2))
     })
+    
+    #LinePlot Shit
+    output$lineplot <- renderPlot({
+      lineplotv1 <- as.integer(strsplit(input$LineData1, ",")[[1]])
+      lineplotv2 <- as.integer(strsplit(input$LineData2, ",")[[1]])
+      linexlab <- input$linexlab
+      lineylab <- input$lineylab
+      
+      #Draw Line Plot
+      plot(x=lineplotv1, y=lineplotv2,
+           xlim=c(0,40),
+           ylim=c(0,199),
+           pch=17,
+           ylab=lineylab,
+           xlab=linexlab,
+           )
+      
+      
+    })
+    
+    #Barplot Shit
+    output$boxplot <- renderPlot({
+      boxplotv1 <- as.integer(strsplit(input$BoxData1, ",")[[1]])
+      boxtitle <- input$BoxData2
+      boxxlab <- input$BoxData3
+      Boxylab <- input$BoxData4
+      
+      
+      #Draw Barplot
+      barplot(boxplotv1,
+              main=boxtitle,
+              xlab=boxxlab,
+              ylab=Boxylab,
+              col="blue",
+              names.arg=c("test1","test2","test3","test4"))
+              
+      
+      
+    })
 }
 
 # Run the application 
 
 
 shinyApp(ui = ui,  server = server)
+
 
